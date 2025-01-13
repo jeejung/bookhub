1a386bd02908ac07a8b372c8ba100807e48713b5
jeejung <hyejee.jung@gmail.com>
2025-01-12 22:58:58 -0800

    Add barcode scanner

--- src/BarcodeScanner.tsx
+++ src/BarcodeScanner.tsx
@@ -1,46 +1,52 @@
-import { useEffect, useState } from "react";
-import { Text, View } from "react-native";
-import { Camera, useCameraDevice } from "react-native-vision-camera";
+import { useEffect, useState } from "react"
+import { Text, View } from "react-native"
+import { Camera, useCameraDevice, useCodeScanner } from "react-native-vision-camera"
 
 export const BarcodeScanner = (): React.JSX.Element => {
     const device = useCameraDevice('back')
-    console.log('device', device);
-    const [cameraPermission, setCameraPermission] = useState<boolean>();
+    const [cameraPermission, setCameraPermission] = useState<boolean>()
 
     const checkCameraPermission = async () => {
-        const status = await Camera.getCameraPermissionStatus();
-        console.log('status', status);
+        const status = await Camera.getCameraPermissionStatus()
+        console.log('status', status)
 
         if (status === 'granted') {
-            setCameraPermission(true);
+            setCameraPermission(true)
         } else if (status === 'not-determined' || status === 'denied') {
             console.log('status2', status)
-            const permission = await Camera.requestCameraPermission();
-            setCameraPermission(permission === 'granted');
+            const permission = await Camera.requestCameraPermission()
+            setCameraPermission(permission === 'granted')
         } else {
-            setCameraPermission(false);
+            setCameraPermission(false)
         }
-    };
+    }
+
+    const codeScanner = useCodeScanner({
+        codeTypes: ['code-128', 'code-39', 'code-93', 'codabar', 'ean-13', 'ean-8', 'itf', 'itf-14', 'upc-e', 'upc-a', 'pdf-417', 'aztec', 'data-matrix'],
+        onCodeScanned: (code) => {
+            console.log('code: ', code)
+        }
+    })
 
     useEffect(() => {
-        checkCameraPermission();
-    }, []);
+        checkCameraPermission()
+    }, [])
 
     if (cameraPermission === null) {
-        return <Text>Checking camera permission...</Text>;
+        return <Text>Checking camera permission...</Text>
     } else if (!cameraPermission) {
-        return <Text>Camera permission not granted</Text>;
+        return <Text>Camera permission not granted</Text>
     }
 
     if (device == null) return <Text>No Device</Text>
 
     return (
         <View style={{ flex: 1 }}>
-            <Text>Real camera!</Text>
             <Camera
                 device={device}
                 isActive={true}
                 style={{ flex: 1, borderWidth: 1, borderColor: 'red', height: 300 }}
+                codeScanner={codeScanner}
             />
         </View>
     )
